9682b5721aa85bcceca1b495b5dce31d28abf1fb
eliseo <davilaeliseo453@gmail.com>
2025-07-02 10:55:24 -0600

    creando header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter,Route,Routes } from "react-router-dom"
 import RegistroLab from "./components/RegistroLab"
 import RegistroLabForm from "./components/RegistroLabForm"
+import Header from "./components/Header"
 function App() {
 
   return (
     <BrowserRouter>
     <div className="container mx-auto">
+      <Header/>
       <Routes>
         <Route path="/" element={<RegistroLab/>} />
         <Route path="/registro-lab" element={<RegistroLabForm/>} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { Link } from "react-router"
+
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <nav className=' py-4 mb-2'>
+        <div className="container mx-auto flex justify-between items-center">
+            <Link to="/" className="text-3xl font-bold">Reservaciones de Laboratorios</Link>
+            <div>
+            <Link to="/registro-lab" className="bg-green-600 text-white px-4 py-2 rounded-lg">Nuevo Registro</Link>
+            </div>
+        </div>        
+    </nav>
   )
 }
