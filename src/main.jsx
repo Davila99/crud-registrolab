0a5f3ed4284cde3f23fdc415144c0acadc3b43e8
eliseo <davilaeliseo453@gmail.com>
2025-07-02 08:48:30 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import RegistroLab from './components/RegistroLab.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RegistroLab />
+    <App />
   </StrictMode>,
 )
