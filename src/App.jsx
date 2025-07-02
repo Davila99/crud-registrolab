2b8b3555152132bfb3af2a7299fac9ffdca47fd6
eliseo <davilaeliseo453@gmail.com>
2025-07-02 08:48:25 -0600

    enrutando rutas con nuevos componentes creados

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-
+import { BrowserRouter,Route,Routes } from "react-router-dom"
+import RegistroLab from "./components/RegistroLab"
+import RegistroLabForm from "./components/RegistroLabForm"
 function App() {
 
   return (
-    <>
-   App
-    </>
+    <BrowserRouter>
+    <div className="container mx-auto">
+      <Routes>
+        <Route path="/" element={<RegistroLab/>} />
+        <Route path="/registro-lab" element={<RegistroLabForm/>} />
+        <Route path="/editar-registro-lab/:id" element={<RegistroLabForm/>} />
+        
+      </Routes>
+    </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/RegistroLabForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RegistroLabForm() {
+  return (
+    <div>RegistroLabForm</div>
+  )
+}
