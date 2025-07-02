e78a18ca78bbe68465203d3d13ab12adc43a10eb
eliseo <davilaeliseo453@gmail.com>
2025-07-02 08:38:56 -0600

    Iterando las etiquetas

--- src/components/RegistroLab.jsx
+++ src/components/RegistroLab.jsx
@@ -15,6 +15,26 @@ export default function RegistroLab() {
     }, [])
     loadRegistroLab()
     return (
-        <div>RegistroLab</div>
+        <div className='mt-8'>
+            <h1 className="text-3xl font-bold text-sky-900">Reservaciones de Laboratorios</h1>
+            <div className='grid grid-cols-1 md:grid-cols-3 mt-5 gap-5 text-white'>
+                {
+                    refistroLabs.map((registro) => (
+                        <div key={registro.id} className="bg-sky-900 p-4 rounded-lg shadow">
+                            <p>{registro.carrera}</p>
+                            <p><span className='font-bold'>Asignatura: </span>{registro.asignatura}</p>
+                            <p><span className='font-bold'>Docente: </span>{registro.docente}</p>
+                            <p><span className='font-bold'>Hora de Inicio: </span>{registro.horaEntrada}</p>
+                            <p><span className='font-bold'>Hora de Fin: </span>{registro.horaSalida}</p>
+                            <div className='mt-4'>
+                                <button className='bg-green-600 text-white px-2 py-1 rounded-lg'>Editar</button>
+                                <button className='bg-red-600 text-white px-2 py-1 rounded-lg ml-2'>Eliminar</button>
+                            </div>
+
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     )
 }
