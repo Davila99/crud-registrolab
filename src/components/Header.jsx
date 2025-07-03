b86fb1cde0ec3d6cdb9de98c030f39a28534a136
eliseo <davilaeliseo453@gmail.com>
2025-07-03 09:58:08 -0600

    actualizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router"
 export default function Header() {
   return (
     <nav className=' py-4 mb-2'>
-        <div className="container mx-auto flex justify-between items-center">
+        {/* <div className="container mx-auto flex justify-between items-center">
             <Link to="/" className="text-3xl font-bold">Reservaciones de Laboratorios</Link>
             <div>
             <Link to="/registro-lab" className="bg-green-600 text-white px-4 py-2 rounded-lg">Nuevo Registro</Link>
             </div>
-        </div>        
+        </div>         */}
     </nav>
   )
 }
--- src/components/RegistroLabForm.jsx
+++ src/components/RegistroLabForm.jsx
@@ -2,6 +2,31 @@ import React from 'react'
 
 export default function RegistroLabForm() {
   return (
-    <div>RegistroLabForm</div>
+    <div>
+        <form>
+            <h1 className="text-3xl font-bold text-sky-900 mb-4">Nueva Reservacion </h1>
+            <div className="mb-4">
+                <label className="block text-sm font-bold text-gray-700">Carrera</label>
+                <input type="text" className="w-full mt-1 p-2 border border-gray-300 rounded" placeholder="Ingrese la carrera" />
+            </div>
+            <div className="mb-4">
+                <label className="block text-sm font-bold text-gray-700">Asignatura</label>
+                <input type="text" className="w-full p-2 border rounded" placeholder="Ingrese la asignatura" />
+            </div>
+            <div className="mb-4">
+                <label className="block text-sm font-bold text-gray-700">Docente</label>
+                <input type="text" className="w-full p-2 border rounded" placeholder="Ingrese el nombre del docente" />
+            </div>
+            <div className="mb-4">
+                <label className="block text-sm font-bold text-gray-700">Hora de Inicio</label>
+                <input type="time" className="w-full p-2 border rounded" />
+            </div>
+            <div className="mb-4">
+                <label className="block text-sm font-bold text-gray-700">Hora de Fin</label>
+                <input type="time" className="w-full p-2 border rounded" />
+            </div>
+            <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded-lg">Guardar Registro</button>
+        </form>
+    </div>
   )
 }
