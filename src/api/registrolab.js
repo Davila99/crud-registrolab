e87086ba03d65ae6ef6e0c46fd7b7a42de6dfcdd
eliseo <davilaeliseo453@gmail.com>
2026-01-08 14:51:25 -0600

    Add API client for activities endpoint
    Introduced a new axios client for the activities support events endpoint and exported a getActivities function to fetch data from it.

--- src/api/registrolab.js
+++ src/api/registrolab.js
@@ -4,3 +4,9 @@ const registroLabApi = axios.create({
   baseURL: "https://davila.pythonanywhere.com/api/registro/"
 });
 export const getRegistros = () => registroLabApi.get();
+
+const activityApi = axios.create({
+  baseURL: "https://uraccan.pythonanywhere.com/api/activitie/eventos-soporte/"
+});
+
+export const getActivities = () => activityApi.get();
